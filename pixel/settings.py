9dfb330787725a22981a5cefe3aa1286beb0fc9e
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-04 13:17:24 -0300

    alterando a chave secreta

--- pixel/settings.py
+++ pixel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_1u*(ka&ff-)tvmwx@wg_adt@#h$rav1*pe(pp(r*^8mp=rtxs'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

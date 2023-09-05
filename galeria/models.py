49b65caa667fdd6edfeb3de71aa39d7e09abd506
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-05 18:11:45 -0300

    consertando erro de digitação no orm banco de dados

--- galeria/models.py
+++ galeria/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Fotografia(models.Model):
     nome = models.CharField(max_length=60, null=False, blank=False)
-    legenfa = models.CharField(max_length=150, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 

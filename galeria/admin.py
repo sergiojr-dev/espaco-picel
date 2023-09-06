99d99c8c9474cd9d7716bc44848e69e1ebd937c9
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-05 23:00:56 -0300

    add a classe admin para filtro do painel do admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+
+class CadastroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'publicada') # aparecer os atributos dentro do admin
+    list_display_links = ('id', 'nome') # colocar o link dentro dos atributos do adin
+    search_fields = ('nome', 'categoria') # campo de busca dentro do admin do django
+    list_filter = ('categoria',) # esta fazendo um filtro por categoria dentro do admin
+    list_editable = ('publicada',) #da a opção de publicar ou despublicar direto no admin
+    list_per_page = 10 # quantidade de itens por página
 
 # Register your models here.
+admin.site.register(Fotografia, CadastroAdmin)

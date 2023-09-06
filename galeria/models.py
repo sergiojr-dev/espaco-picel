5d9e6a5ddfb2d746d4d413902830237e27b14ad8
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-09-06 15:25:56 -0300

    atualização do models.py para o banco de dados aceitar o envio de fotos pelo usuário e o requiriments.txt recebeu duas bibliotecas nova o pillow typing_extensions.

--- galeria/models.py
+++ galeria/models.py
@@ -16,9 +16,10 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIAS, default='')
     #choice serve para dizer que serão categorias pré definidas e selecionaveis e default para dizer em qual ela inicia
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data = models.DateTimeField(default=datetime.now, blank=False)
+
 
 
     def __str__(self):
